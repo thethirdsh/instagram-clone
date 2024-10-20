61089c89fd4bf89e4d299581ac0cb85a1e822376
momen yasser <momenyasser@itworx.com>
2024-10-20 04:11:39 +0300

    Refactor image sizes and button styling in story and post components

--- client/src/app/profile/page.tsx
+++ client/src/app/profile/page.tsx
@@ -29,216 +29,243 @@ const Profile = () => {
           <div className="border-l border-gray-200 h-screen"></div>
         </div>
       )}
-      <div className="flex flex-col justify-center items-center w-full h-full">
-        <div className="flex flex-col pt-6 ml-48">
-          <div className="flex flex-row items-center justify-center items-center mb-12">
-            <div className="flex w-52 h-52 items-center">
-              <div className="w-36 h-36 rounded-full bg-gradient-to-tr from-pink-500 to-yellow-500 flex items-center justify-center p-[2px]">
-                <button className="w-full h-full rounded-full bg-white flex items-center justify-center">
-                  <Image
-                    src="/images/profile.png"
-                    width={130}
-                    height={130}
-                    alt="Picture of the author"
-                  />
-                </button>
-              </div>
-            </div>
-            <div className="flex flex-col mt-5">
-              <div className="flex flex-row mb-6">
-                <p className="text-xl mr-5">username</p>
-                <div className="flex flex-row gap-2">
-                  <button className="font-medium rounded-lg bg-gray-200 text-sm px-4 py-1">
-                    Following
-                  </button>
-                  <button className="font-medium rounded-lg bg-gray-200 text-sm px-4 py-1">
-                    Message
+      <div className="flex flex-col justify-center items-center h-full">
+        <div className="flex flex-col justify-center items-center pt-6">
+          <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-row mb-11 pl-32 md:pl-14 w-[54rem]">
+              <div className="hidden md:flex items-center">
+                <div className="w-20 h-20 md:w-40 md:h-40 rounded-full bg-gradient-to-tr from-pink-500 to-yellow-500 flex items-center justify-center p-[2px]">
+                  <button className="relative w-[98%] h-[98%] rounded-full bg-white flex items-center justify-center">
+                    <Image
+                      className="rounded-full border-4 border-white"
+                      src="/images/profile.png"
+                      fill={true}
+                      alt="Picture of the author"
+                    />
                   </button>
                 </div>
+              </div>
+              <div className="flex flex-col mt-4 pl-24 w-full">
+                <div className="flex flex-row mb-6">
+                  <div className="flex md:hidden items-center pr-4">
+                    <div className="w-8 h-8 rounded-full bg-gradient-to-tr from-pink-500 to-yellow-500 flex items-center justify-center p-[2px]">
+                      <button className="relative w-[98%] h-[98%] rounded-full bg-white flex items-center justify-center">
+                        <Image
+                          className="rounded-full border-4 border-white"
+                          src="/images/profile.png"
+                          fill={true}
+                          alt="Picture of the author"
+                        />
+                      </button>
+                    </div>
+                  </div>
+                  <p className="text-xl mr-5">username</p>
+                  <div className="flex flex-row gap-2">
+                    <button className="font-medium rounded-lg bg-gray-200 text-sm px-4 py-1">
+                      Following
+                    </button>
+                    <button className="font-medium rounded-lg bg-gray-200 text-sm px-4 py-1">
+                      Message
+                    </button>
+                  </div>
 
-                <button className="pl-2">
-                  <BsThreeDots />
-                </button>
+                  <button className="pl-2">
+                    <BsThreeDots />
+                  </button>
+                </div>
+                <div className="flex flex-row mb-3">
+                  <p className="mr-10">
+                    <span className="font-semibold">560</span> posts
+                  </p>
+                  <p className="mr-10">
+                    <span className="font-semibold">250</span> followers
+                  </p>
+                  <p>
+                    <span className="font-semibold">420</span> following
+                  </p>
+                </div>
+                <div className="flex flex-col">
+                  <p className="font-semibold">Full Name</p>
+                  <p className="text-gray-500">Public figure</p>
+                  <p className="text-balance text-xs md:text-base w-[60%] md:w-[70%] lg:w-full lg:text-base">
+                    A little description written by the account owner or a big
+                    one. I don&apos;t know.
+                  </p>
+                </div>
+                <p className="text-xs text-gray-500 pt-3">
+                  Followed by username, username + 5 more
+                </p>
               </div>
-              <div className="flex flex-row mb-3">
-                <p className="mr-10">
-                  <span className="font-semibold">560</span> posts
-                </p>
-                <p className="mr-10">
-                  <span className="font-semibold">250</span> followers
-                </p>
-                <p>
-                  <span className="font-semibold">420</span> following
-                </p>
-              </div>
-              <div className="flex flex-col">
-                <p className="font-semibold">Full Name</p>
-                <p className="text-gray-500">Public figure</p>
-                <p>
-                  A little description written by the account owner or a big
-                  one. I don&apos;t know
+            </div>
+            <div className="flex flex-row overflow-x-auto mb-14 pl-10 gap-3 md:gap-14 w-[60%] md:w-[90%] lg:w-full">
+              <div className="flex flex-col space-y-2">
+                <div className="w-16 h-16 md:w-20 md:h-20 rounded-full border-2 border-gray-200 flex items-center justify-center p-[0.5px]">
+                  <button className="relative rounded-full bg-white flex items-center justify-center w-[94%] h-[94%]">
+                    <Image
+                      src="/images/profile.png"
+                      fill={true}
+                      alt="Picture of the author"
+                    />
+                  </button>
+                </div>
+                <p className="text-center text-xs md:text-sm truncate pt-2 w-16">
+                  Highlight
                 </p>
               </div>
-              <p className="text-xs text-gray-500 pt-3">
-                Followed by username, username + 5 more
-              </p>
+              <div className="flex flex-col items-center space-y-2">
+                <div className="w-16 h-16 md:w-20 md:h-20 rounded-full border-2 border-gray-200 flex items-center justify-center p-[0.5px]">
+                  <button className="relative rounded-full bg-white flex items-center justify-center w-[94%] h-[94%]">
+                    <Image
+                      src="/images/profile.png"
+                      fill={true}
+                      alt="Picture of the author"
+                    />
+                  </button>
+                </div>
+                <p className="text-center text-xs md:text-sm truncate pt-2 w-16">
+                  Highlight
+                </p>
+              </div>
+              <div className="flex flex-col items-center space-y-2">
+                <div className="w-16 h-16 md:w-20 md:h-20 rounded-full border-2 border-gray-200 flex items-center justify-center p-[0.5px]">
+                  <button className="relative rounded-full bg-white flex items-center justify-center w-[94%] h-[94%]">
+                    <Image
+                      src="/images/profile.png"
+                      fill={true}
+                      alt="Picture of the author"
+                    />
+                  </button>
+                </div>
+                <p className="text-center text-xs md:text-sm truncate pt-2 w-16">
+                  Highlight
+                </p>
+              </div>
+              <div className="flex flex-col items-center space-y-2">
+                <div className="w-16 h-16 md:w-20 md:h-20 rounded-full border-2 border-gray-200 flex items-center justify-center p-[0.5px]">
+                  <button className="relative rounded-full bg-white flex items-center justify-center w-[94%] h-[94%]">
+                    <Image
+                      src="/images/profile.png"
+                      fill={true}
+                      alt="Picture of the author"
+                    />
+                  </button>
+                </div>
+                <p className="text-center text-xs md:text-sm truncate pt-2 w-16">
+                  Highlight
+                </p>
+              </div>
+              <div className="flex flex-col items-center space-y-2">
+                <div className="w-16 h-16 md:w-20 md:h-20 rounded-full border-2 border-gray-200 flex items-center justify-center p-[0.5px]">
+                  <button className="relative rounded-full bg-white flex items-center justify-center w-[94%] h-[94%]">
+                    <Image
+                      src="/images/profile.png"
+                      fill={true}
+                      alt="Picture of the author"
+                    />
+                  </button>
+                </div>
+                <p className="text-center text-xs md:text-sm truncate pt-2 w-16">
+                  Highlight
+                </p>
+              </div>
+              <div className="flex flex-col items-center space-y-2">
+                <div className="w-16 h-16 md:w-20 md:h-20 rounded-full border-2 border-gray-200 flex items-center justify-center p-[0.5px]">
+                  <button className="relative rounded-full bg-white flex items-center justify-center w-[94%] h-[94%]">
+                    <Image
+                      src="/images/profile.png"
+                      fill={true}
+                      alt="Picture of the author"
+                    />
+                  </button>
+                </div>
+                <p className="text-center text-xs md:text-sm truncate pt-2 w-16">
+                  Highlight
+                </p>
+              </div>
             </div>
           </div>
-          <div className="flex flex-row mb-14">
-            <div className="flex flex-col items-center space-y-2 w-full h-full">
-              <div className="w-20 h-20 rounded-full bg-gray-200 flex items-center justify-center p-[2px]">
-                <button className="w-full h-full rounded-full bg-white flex items-center justify-center">
-                  <Image
-                    src="/images/profile.png"
-                    width={71}
-                    height={71}
-                    alt="Picture of the author"
-                  />
-                </button>
-              </div>
-              <p className="text-center text-sm pt-2">Highlight</p>
-            </div>
-            <div className="flex flex-col items-center space-y-2 w-full h-full">
-              <div className="w-20 h-20 rounded-full bg-gray-200 flex items-center justify-center p-[2px]">
-                <button className="w-full h-full rounded-full bg-white flex items-center justify-center">
-                  <Image
-                    src="/images/profile.png"
-                    width={71}
-                    height={71}
-                    alt="Picture of the author"
-                  />
-                </button>
-              </div>
-              <p className="text-center text-sm pt-2">Highlight</p>
-            </div>
-            <div className="flex flex-col items-center space-y-2 w-full h-full">
-              <div className="w-20 h-20 rounded-full bg-gray-200 flex items-center justify-center p-[2px]">
-                <button className="w-full h-full rounded-full bg-white flex items-center justify-center">
-                  <Image
-                    src="/images/profile.png"
-                    width={71}
-                    height={71}
-                    alt="Picture of the author"
-                  />
-                </button>
-              </div>
-              <p className="text-center text-sm pt-2">Highlight</p>
-            </div>
-            <div className="flex flex-col items-center space-y-2 w-full h-full">
-              <div className="w-20 h-20 rounded-full bg-gray-200 flex items-center justify-center p-[2px]">
-                <button className="w-full h-full rounded-full bg-white flex items-center justify-center">
-                  <Image
-                    src="/images/profile.png"
-                    width={71}
-                    height={71}
-                    alt="Picture of the author"
-                  />
-                </button>
-              </div>
-              <p className="text-center text-sm pt-2">Highlight</p>
-            </div>
-            <div className="flex flex-col items-center space-y-2 w-full h-full">
-              <div className="w-20 h-20 rounded-full bg-gray-200 flex items-center justify-center p-[2px]">
-                <button className="w-full h-full rounded-full bg-white flex items-center justify-center">
-                  <Image
-                    src="/images/profile.png"
-                    width={71}
-                    height={71}
-                    alt="Picture of the author"
-                  />
-                </button>
-              </div>
-              <p className="text-center text-sm pt-2">Highlight</p>
-            </div>
-            <div className="flex flex-col items-center space-y-2 w-full h-full">
-              <div className="w-20 h-20 rounded-full bg-gray-200 flex items-center justify-center p-[2px]">
-                <button className="w-full h-full rounded-full bg-white flex items-center justify-center">
-                  <Image
-                    src="/images/profile.png"
-                    width={71}
-                    height={71}
-                    alt="Picture of the author"
-                  />
-                </button>
-              </div>
-              <p className="text-center text-sm pt-2">Highlight</p>
-            </div>
-          </div>
+
           <div className="flex flex-col justify-center mb-6 w-full">
             <hr className="border-t-1" />
             <div className="flex flex-row justify-center">
               <button className="flex flex row items-center mr-16 pt-4">
                 <MdOutlineGridOn className="text-xs text-gray-500" />
-                <p className="ml-1.5 text-xs text-gray-500">POSTS</p>
+                <p className="hidden md:flex ml-1.5 text-xs text-gray-500">
+                  POSTS
+                </p>
               </button>
               <button className="flex flex row items-center mr-16 pt-4">
                 <BiMoviePlay className="text-sm text-gray-500" />
-                <p className="ml-1.5 text-xs text-gray-500">REELS</p>
+                <p className="hidden md:flex ml-1.5 text-xs text-gray-500">
+                  REELS
+                </p>
               </button>
               <button className="flex flex row items-center pt-4">
                 <LiaUserTagSolid className="text-sm text-gray-500" />
-                <p className="ml-1.5 text-xs text-gray-500">TAGGED</p>
+                <p className="hidden md:flex ml-1.5 text-xs text-gray-500">
+                  TAGGED
+                </p>
               </button>
             </div>
           </div>
-          <div className="grid grid-cols-3 gap-1 justify-center items-center">
-            <div className="relative w-72 h-72">
+          <div className="grid grid-cols-3 gap-1 w-[45%] md:w-[90%] lg:w-[100%]">
+            <div className="relative size-[8rem] md:size-[16rem] lg:size-[18rem]">
               <Image
                 src="/images/post.jpg"
                 fill={true}
                 alt="Picture of the author"
               />
             </div>
-            <div className="relative w-72 h-72">
+            <div className="relative size-[8rem] md:size-[16rem] lg:size-[18rem]">
               <Image
                 src="/images/post.jpg"
                 fill={true}
                 alt="Picture of the author"
               />
             </div>
-            <div className="relative w-72 h-72">
+            <div className="relative size-[8rem] md:size-[16rem] lg:size-[18rem]">
               <Image
                 src="/images/post.jpg"
                 fill={true}
                 alt="Picture of the author"
               />
             </div>
-            <div className="relative w-72 h-72">
+            <div className="relative size-[8rem] md:size-[16rem] lg:size-[18rem]">
               <Image
                 src="/images/post.jpg"
                 fill={true}
                 alt="Picture of the author"
               />
             </div>
-            <div className="relative w-72 h-72">
+            <div className="relative size-[8rem] md:size-[16rem] lg:size-[18rem]">
               <Image
                 src="/images/post.jpg"
                 fill={true}
                 alt="Picture of the author"
               />
             </div>
-            <div className="relative w-72 h-72">
+            <div className="relative size-[8rem] md:size-[16rem] lg:size-[18rem]">
               <Image
                 src="/images/post.jpg"
                 fill={true}
                 alt="Picture of the author"
               />
             </div>
-            <div className="relative w-72 h-72">
+            <div className="relative size-[8rem] md:size-[16rem] lg:size-[18rem]">
               <Image
                 src="/images/post.jpg"
                 fill={true}
                 alt="Picture of the author"
               />
             </div>
-            <div className="relative w-72 h-72">
+            <div className="relative size-[8rem] md:size-[16rem] lg:size-[18rem]">
               <Image
                 src="/images/post.jpg"
                 fill={true}
                 alt="Picture of the author"
               />
             </div>
-            <div className="relative w-72 h-72">
+            <div className="relative size-[8rem] md:size-[16rem] lg:size-[18rem]">
               <Image
                 src="/images/post.jpg"
                 fill={true}
