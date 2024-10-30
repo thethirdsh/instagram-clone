2d25784e19b9a819791e94fdc59f3de80a349090
momen yasser <momenyasser@itworx.com>
2024-10-31 00:05:59 +0300

    implemented signup page and fixed some issues with the login page

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,36 +1,52 @@
-import Link from "next/link"
+import Footer from '@/components/footer'
+import Link from 'next/link'
 
 const Login = () => {
   return (
-    <div className="flex flex-col justify-center items-center bg-white text-black w-full h-screen">
-      <div className="flex flex-col items-center border-[1px] border-gray-300 w-[350px] h-[409px]">
-        <p className="text-5xl font-semibold text-black font-dancingScript pt-8">
-          Instagram
-        </p>
-        <form className="flex flex-col items-center pt-14 w-full h-[50%]">
-          <input
-            type="text"
-            placeholder="Phone number, username, or email"
-            className="border-[1px] border-gray-300 rounded w-[80%] text-black text-xs font-light bg-[#FAFAFA] pl-2.5 h-10 focus:outline-gray-500"
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            className="border-[1px] border-gray-300 rounded w-[80%] text-xs font-light bg-[#FAFAFA] mt-2 pl-2.5 h-10 focus:outline-gray-500"
-          />
-          <button
-            type="submit"
-            className="flex justify-center items-center bg-[#0099FF] rounded-lg w-[80%] mt-4 h-8"
-          >
-            <p className="text-white text-sm font-medium">Log in</p>
-          </button>
-        </form>
-        <div className="flex flex-row items-center gap-5 pt-4 w-[80%]">
-          <hr className="w-[39%] border-gray-300"/>
-          <p className="text-sm font-medium text-gray-500">OR</p>
-          <hr className="w-[39%] border-gray-300"/>
+    <div className="flex flex-col bg-white text-black w-full h-full">
+      <div className="flex flex-col justify-center items-center pt-20 lg:pt-52">
+        <div className="flex flex-col items-center border-[1px] border-gray-300 w-[350px] h-[409px]">
+          <p className="text-5xl font-semibold text-black font-dancingScript pt-8">
+            Instagram
+          </p>
+          <form className="flex flex-col items-center pt-14 w-full h-[50%]">
+            <input
+              type="text"
+              placeholder="Phone number, username, or email"
+              className="border-[1px] border-gray-300 rounded w-[80%] text-black text-xs font-light bg-[#FAFAFA] pl-2.5 h-10 focus:outline-gray-500"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="border-[1px] border-gray-300 rounded w-[80%] text-xs font-light bg-[#FAFAFA] mt-2 pl-2.5 h-10 focus:outline-gray-500 "
+            />
+            <button
+              type="submit"
+              className="flex justify-center items-center bg-[#0099FF] rounded-lg w-[80%] mt-4 h-8"
+            >
+              <p className="text-white text-sm font-medium">Log in</p>
+            </button>
+          </form>
+          <div className="flex flex-row items-center gap-5 pt-4 w-[80%]">
+            <hr className="w-[39%] border-gray-300" />
+            <p className="text-sm font-medium text-gray-500">OR</p>
+            <hr className="w-[39%] border-gray-300" />
+          </div>
+          <Link className="pt-8" href="/forgot-password">
+            <p className="text-[#003366] text-sm font-light">
+              Forgot password?
+            </p>
+          </Link>
         </div>
-        <Link className="pt-8" href="/forgot-password"><p className="text-[#003366] text-sm font-light">Forgot password?</p></Link>
+        <div className="flex justify-center items-center border-[1px] border-gray-300 text-sm mt-2.5 w-[350px] h-16">
+          <Link href="/signup" className="">
+            Don&apos;t have an account?
+            <span className="font-semibold text-[#0099FF]"> Sign up</span>
+          </Link>
+        </div>
+      </div>
+      <div className="hidden lg:flex pt-36">
+        <Footer />
       </div>
     </div>
   )
---
+++ client/src/app/signup/page.tsx
@@ -0,0 +1,81 @@
+import Footer from '@/components/footer'
+
+const Signup = () => {
+  return (
+    <div className="flex flex-col w-full h-full">
+      <div className="flex flex-col justify-center items-center pt-10 lg:pt-24">
+        <div className="flex flex-col justify-center items-center border-[1px] border-gray-300 w-[350px] h-[510px]">
+          <p className="text-5xl font-semibold text-black font-dancingScript pb-2">
+            Instagram
+          </p>
+          <p className="text-center font-semibold text-gray-500 pt-4 w-[80%]">
+            {' '}
+            Sign up to see photos and videos from your friends.
+          </p>
+          <form className="flex flex-col justify-center items-center pt-2">
+            <input
+              type="text"
+              placeholder="Mobile Number or Email"
+              className="border-[1px] border-gray-300 rounded w-[80%] text-black text-xs font-light bg-[#FAFAFA] pl-2.5 h-10 focus:outline-gray-500"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="border-[1px] border-gray-300 rounded w-[80%] text-black text-xs font-light bg-[#FAFAFA] pl-2.5 mt-1.5 h-10 focus:outline-gray-500"
+            />
+            <input
+              type="text"
+              placeholder="Full Name"
+              className="border-[1px] border-gray-300 rounded w-[80%] text-black text-xs font-light bg-[#FAFAFA] pl-2.5 mt-1.5 h-10 focus:outline-gray-500"
+            />
+            <input
+              type="text"
+              placeholder="Username"
+              className="border-[1px] border-gray-300 rounded w-[80%] text-black text-xs font-light bg-[#FAFAFA] pl-2.5 mt-1.5 h-10 focus:outline-gray-500"
+            />
+            <p className="text-center text-xs text-gray-500 text-light pt-4 w-[80%]">
+              People who use our service may have uploaded your contact
+              information to Instagram.{' '}
+              <span className="text-[#003366] text-xs font-light">
+                {' '}
+                Learn More
+              </span>
+            </p>
+            <p className="text-center text-xs text-gray-500 text-light pt-4 w-[80%]">
+              By signing up, you agree to our{' '}
+              <span className="text-[#003366] text-xs font-light"> Terms </span>{' '}
+              ,{' '}
+              <span className="text-[#003366] text-xs font-light">
+                {' '}
+                Privacy Policy
+              </span>{' '}
+              and{' '}
+              <span className="text-[#003366] text-xs font-light">
+                {' '}
+                Cookies Policy{' '}
+              </span>{' '}
+              .
+            </p>
+            <button
+              type="submit"
+              className="flex justify-center items-center bg-[#0099FF] rounded-lg w-[80%] mt-4 h-8"
+            >
+              <p className="text-white text-sm font-medium">Sign up</p>
+            </button>
+          </form>
+        </div>
+        <div className="flex justify-center items-center border-[1px] border-gray-300 text-sm mt-2.5 w-[350px] h-16">
+          <p>
+            Have an account?
+            <span className="font-semibold text-[#0099FF]"> Log in</span>
+          </p>
+        </div>
+      </div>
+      <div className="hidden lg:flex pt-36">
+        <Footer />
+      </div>
+    </div>
+  )
+}
+
+export default Signup
